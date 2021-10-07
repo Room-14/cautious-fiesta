4032afa31b74c191d7493f449b95865869d9064a
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2021-10-07 11:13:17 +0000

    added rectricted auth access functionality

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,22 @@
 const jwt = require('jsonwebtoken')
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    // check jwt exists and is valid
+    if (token) {
+        jwt.verify(token, 'appAttendance Secrete', (error, decodedToken) => {
+            if (error) {
+                console.log(error.message)
+                res.redirect('/signup')
+            } else {
+                console.log(decodedToken)
+                next()
+            }
+        })
+    } else {
+        res.redirect('/signup')
+    }
+}
+
+module.exports = { requireAuth }
